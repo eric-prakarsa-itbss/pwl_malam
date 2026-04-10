02bf23f53ea9e10221e49045a7e812de19269f41
Eric Prakarsa Putra <shiro.noname@gmail.com>
2026-04-10 20:35:17 +0700

    pertemuan 6

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'mahasiswas';
+
+    protected $fillable = [
+        'fullname',
+        'NIM',
+        'NIDN',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'alamat'
+    ];
 }
